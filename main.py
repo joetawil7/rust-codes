739b0c3b1279a20c9d6354c9554ed9ea835c0215
Joetawil7 <joetawil7@hotmail.com>
2022-05-10 20:34:17 +0200

    new

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-# This example requires the 'message_content' privileged intents
-
-import os
+import random
 import discord
-from discord.ext import commands
-
-
 intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+intents.members = True
 
+from discord.ext import commands
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print("The bot is ready")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@bot.command(pass_context = True)
+async def newcode(ctx):
+    global x 
+    x = random.randint(1000,9999)
+    await ctx.send('New Code generated!')
+    await ctx.message.author.send("New Code: " + str(x))
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+@bot.command(pass_context = True)
+async def lastcode(ctx):
+    await ctx.message.author.send("Last Code: " + str(x))
 
+@bot.command(pass_context = True)
+async def newlocker(ctx):
+    if ctx.guild.owner == ctx.message.author:
+      global y 
+      y = random.randint(1000,9999)
+      await ctx.guild.owner.send(y)
 
-bot.run(os.environ["DISCORD_TOKEN"])
+@bot.command(pass_context=True)
+async def lastlocker(ctx):
+  await ctx.guild.owner.send(ctx.message.author.name + " has asked for the locker's code")
+  await ctx.message.author.send("Locker Code: " + str(y))
+
+bot.run('OTczNjEwOTYxNzYyMTI3ODky.GHJvwr.-GsVGHkpWLB52dF9L7AD79M3mr3vHJE9fq558A')
+
